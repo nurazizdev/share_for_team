9ffa1f8f9da188e59b64b0d9cc1c4c38e79d6ac0
Nuraziz <ungboyevn@gmail.com>
2024-04-17 21:32:16 +0500

    first commit

---
+++ share.local/index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Share</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="shortcut icon" href="assets/img/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+<div class="header">
+    <div class="container">
+        <nav class="navbar navbar-expand-sm">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src="assets/img/greenlg.png" alt="logo" width="100px"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"         aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+              <div class="navbar-nav">
+                <a class="nav-link active main-link" aria-current="page" href="#">Asosiy</a>
+                <a class="nav-link" href="#">Biz haqimizda</a>
+                <a class="nav-link" href="#">Kurslar</a>
+                <a class="nav-link" href="#">Natijalar</a>
+                <a class="nav-link" href="#">Bizning Jamoa</a>
+              </div>
+            </div>
+          </div>
+        </nav>
+
+        <div class="head-content d-flex justify-content-between align-items-center">
+            <div class="left w-50">
+              <h1 class="display-4">Biz ilmni <span>BEPULGA</span> ulashamiz!</h1>
+              <h5 class="mt-3">"Share" loyihasining maqsadi ta'limga muhtoj yoshlarni qo'llab–quvvatlash va ularni bepul o'qitishdir!</h5>
+              <button type="button" class="btn mt-3">Kurslarga Yozilish</button>
+            </div>
+            <div class="right w-50">
+                <img src="assets/img/online-bro.svg" alt="online" width="100%">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="main">
+      <div class="main-intro mib py-5">
+        <div class="container">
+          <h1 class="text-center">Biz Haqimizda</h1>
+          <p class="text-center w-50 m-auto mt-4" style="font-size:1.1rem;">"SHARE Loyihasi" O'zbekistondagi eng muvaffaqiyatli tashabbuslardan biri bo'lib, uning maqsadi shijoatli va aqlli insonlar uchun oltin imkoniyat yaratishdir. Loyihaning asosiy maqsadi TA'LIMGA MUHTOJ YOSHLARNI QO'LLAB – quvvatlash va ularni bepul o'qitishdir.</p>
+        </div>
+      </div>
+      <div class="main-intro py-5">
+        <div class="container">
+          <h1 class="text-center">Loyihamiz o'z ichiga nimalarni oladi?</h1>
+          <div class="row mt-4">
+            <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 my-2">
+                <div class="card" style="width: 100%">
+                    <img src="assets/img/studentonline.jpg" class="card-img-top" alt="">
+                    <div class="card-body">
+                      <h5 class="card-title">IELTS va SAT kurslari</h5>
+                      <p class="card-text">"Share" loyihasi bilan birgalikda IELTS va SAT kurslarini tajribali ustozlarimizdan bepulga o'rganib, dunyoning TOP universitetlariga o'qishga kirish imkoniyatingizni oshiring!</p>
+                      <a href="#" class="btn align-bottom">Batafsil</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 my-2">
+                <div class="card" style="width: 100%;">
+                    <img src="assets/img/Podcast-bro.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Turli xil Podkastlar</h5>
+                      <p class="card-text">Yoshlar uchun qiziq mavzularda biz mashhur insonlar bilan onlayn podkastlar uyushtirishimiz orqali, ularning bilimlarini va dunyoqarashlarini kengaytiramiz!</p>
+                      <a href="#" class="btn align-bottom">Batafsil</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 my-2">
+                <div class="card" style="width: 100%;">
+                    <img src="assets/img/event.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Onlayn Tadbirlar</h5>
+                      <p class="card-text">Bu rubrikamizda biz Telegram kanalimizda bepul speaking mocklar, speaking klublar, kunlik so'zlar, o'yinlar, va shu kabi tadbirlarni davomiy tashkillashtiramiz</p>
+                      <a href="#" class="btn align-bottom">Batafsil</a>
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="teachers">
+        <div class="container">
+          <h1 class="text-center">Bizning ustozlarimiz</h1>
+        </div>
+      </div>
+<div>
+
+
+
+    <script src="assets/js/main.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
